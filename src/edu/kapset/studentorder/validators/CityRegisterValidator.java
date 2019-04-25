69607722e96a446157f7e67dfae8b40a92bb0ee1
artemkapset <artemkapset@mail.ru>
2019-04-25 21:52:41 +0300

    Start to work with interfaces (1st interface CityRegisterChecker was created)

--- src/edu/kapset/studentorder/validators/CityRegisterValidator.java
+++ src/edu/kapset/studentorder/validators/CityRegisterValidator.java
@@ -5,14 +5,22 @@ import edu.kapset.studentorder.domain.StudentOrder;
 
 // класс, содержащий в себе логику проверки регистрации гражданина в ГРН
 public class CityRegisterValidator {
-    String hostName;
-    String login;
+    public String hostName;
+    protected int port;
+    private String login;
     String password;
+    private CityRegisterChecker personChecker;
+
+    public CityRegisterValidator() {
+        personChecker = new FakeCityRegisterChecker();
+    }
 
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
-        System.out.println("City register is running: " + hostName + "," + login + "," + password);
+        personChecker.checkPerson(so.getHusband());
+        personChecker.checkPerson(so.getWife());
+        personChecker.checkPerson(so.getChild());
+
         AnswerCityRegister ans = new AnswerCityRegister();
-        ans.success = false;
         return ans;
     }
 }
